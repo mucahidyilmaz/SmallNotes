fe371a4a96a8d3b570b5e05072c3d7c32a8e90d1
mucahid yilmaz <330179@ogr.ktu.edu.tr>
2020-08-31 14:05:05 +0300

    Initial commit

---
+++ app/src/main/java/muteas19/smallnote/auth/Login.java
@@ -0,0 +1,138 @@
+package muteas19.smallnote.auth;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import muteas19.smallnote.MainActivity;
+import muteas19.smallnote.R;
+
+public class Login extends AppCompatActivity {
+    EditText lEmail,lPassword;
+    Button loginNow;
+    TextView forgetPass,createAcc;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    FirebaseUser user;
+    ProgressBar spinner;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("SmallNote'a Bağlan");
+
+        lEmail = findViewById(R.id.email);
+        lPassword = findViewById(R.id.lPassword);
+        loginNow = findViewById(R.id.loginBtn);
+
+        spinner = findViewById(R.id.progressBar3);
+
+        forgetPass = findViewById(R.id.forgotPasword);
+        createAcc = findViewById(R.id.createAccount);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+
+        showWarning();
+
+        loginNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mEmail = lEmail.getText().toString();
+                String mPassword = lPassword.getText().toString();
+
+                if(mEmail.isEmpty() || mPassword.isEmpty()){
+                    Toast.makeText(Login.this, "Alanları Doldurmak Zorunludur.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // delete notes first
+
+                spinner.setVisibility(View.VISIBLE);
+
+                if(fAuth.getCurrentUser().isAnonymous()){
+                    FirebaseUser user = fAuth.getCurrentUser();
+
+                    fStore.collection("notes").document(user.getUid()).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(Login.this, "Tüm Geçici Notlar Silindi.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    // delete Temp user
+
+                    user.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(Login.this, "Geçici kullanıcı Silindi.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                fAuth.signInWithEmailAndPassword(mEmail,mPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(Login.this, "Başarılı !", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, "Bağlantı Başarısız. " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        spinner.setVisibility(View.GONE);
+                    }
+                });
+            }
+        });
+
+        createAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+            }
+        });
+    }
+
+    private void showWarning() {
+        final AlertDialog.Builder warning = new AlertDialog.Builder(this)
+                .setTitle("Emin Misiniz ?")
+                .setMessage("Mevcut Hesabın Bağlanması Tüm geçici notları siler. Kaydetmek için Yeni Hesap Oluşturun.")
+                .setPositiveButton("Not Kaydedildi", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(getApplicationContext(),Register.class));
+                        finish();
+                    }
+                }).setNegativeButton("Anlaşıldı", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+        warning.show();
+    }
+}
---
+++ app/src/main/java/muteas19/smallnote/note/NoteDetails.java
@@ -0,0 +1,64 @@
+package muteas19.smallnote.note;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.text.method.ScrollingMovementMethod;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import muteas19.smallnote.R;
+
+public class NoteDetails extends AppCompatActivity {
+    Intent data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_details);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        data = getIntent();
+
+
+        TextView content = findViewById(R.id.noteDetailsContent);
+        TextView title = findViewById(R.id.noteDetailsTitle);
+        content.setMovementMethod(new ScrollingMovementMethod());
+
+        content.setText(data.getStringExtra("içerik"));
+        title.setText(data.getStringExtra("başlık"));
+        content.setBackgroundColor(getResources().getColor(data.getIntExtra("kod",0),null));
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(view.getContext(),EditNote.class);
+                i.putExtra("başlık",data.getStringExtra("başlık"));
+                i.putExtra("içerik",data.getStringExtra("içerik"));
+                i.putExtra("noteId",data.getStringExtra("noteId"));
+                startActivity(i);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
